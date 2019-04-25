48226f42ea757b57f926280c62ab1fe94c67262f
Zach Schuermann <zachary.zvs@gmail.com>
2019-04-24 23:05:43 -0500

    deploy fix

--- deploy.sh
+++ deploy.sh
@@ -10,10 +10,11 @@ make image
 echo $DOCKER_PW | base64 --decode -i > ${HOME}/password.txt
 cat ~/password.txt | docker login --username ${DOCKER_USERNAME} --password-stdin
 
-docker push gcr.io/ou-reviews/stev-api:latest
-
 echo $GCLOUD_SERVICE_KEY | base64 --decode -i > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+
+echo "y" | gcloud auth configure-docker
+docker push gcr.io/ou-reviews/stev-api:latest
 
 gcloud --quiet config set project $PROJECT_NAME
 # gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
