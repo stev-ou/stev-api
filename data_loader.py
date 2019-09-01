11e2d9f77ca9a25d2441aa865896d79a1de7bbe1
Sam Jett <samjett247@gmail.com>
2019-08-31 21:08:11 -0700

    Big Changes to data loader and data aggregation to accept new format and to simplify code

--- data_loader.py
+++ data_loader.py
@@ -10,9 +10,9 @@ from mongo import mongo_driver as db_conn
 from data_aggregation import aggregate_data
 
 # Define the name of the database and the name of the collection. Insert each .csv record as a document within the collection
-DB_NAME = "test-agg-db" # practice
-OCR_DB_NAME = 'ocr_db_v1'
-ocr_collections = ['CoA']#, 'CoAaS', 'CoA&GS', 'CoCE-DoA', 'MFPCoB', 'MCoEaE', 'JRCoE', 'GCoE', 'WFCoFA', 'HC', 'CoIS', 'GCoJaMC', 'CoPaCS', 'UC', 'CfIaDL', 'EWP', 'R-AF']
+DB_NAME = "reviews-db-v1" # practice
+OCR_DB_NAME = 'ocr_db_v4'
+ocr_collections = ['reviews']#, 'CoAaS', 'CoA&GS', 'CoCE-DoA', 'MFPCoB', 'MCoEaE', 'JRCoE', 'GCoE', 'WFCoFA', 'HC', 'CoIS', 'GCoJaMC', 'CoPaCS', 'UC', 'CfIaDL', 'EWP', 'R-AF']
 
 ### DEBUG - force_update is always true - off in prod
 def update_database(force_update=False):
@@ -35,35 +35,28 @@ def update_database(force_update=False):
         print('Converting the scraped collection '+ocr_coll+ ' to pd dataframe.')
         ocr_db = conn.get_db_collection(OCR_DB_NAME, ocr_coll)
         df = pd.DataFrame(list(ocr_db.find()))
-        df.drop(['_id'],axis=1, inplace=True).rename(columns ={'Individual Responses':'Responses'}, inplace=True)
+        # Condition the df
+        ## Cleaning the data prior to aggregation
+        df = df.drop(['_id'],axis=1, errors = 'ignore').rename(columns ={'Individual Responses':'Responses'})
         df['Instructor ID'] = (df['Instructor First Name']+df['Instructor Last Name']).apply(str).apply(hash).astype('int32').abs()
+        df['course_uuid'] = (df['Subject Code']+df['Course Number'].apply(str)+df['Section Title'].apply(lambda x: x[:-4])).apply(str).apply(hash).astype('int32').abs().apply(str) 
         df['Question Number'] = df['Question Number'].astype(int)
         df['Term Code'] = df['Term Code'].astype(int)
         # Make sure the First and Last names are in camelcase; i.e. no CHUNG-HAO LEE
         df['Instructor First Name'] = df['Instructor First Name'].apply(str.title)
         df['Instructor Last Name'] = df['Instructor Last Name'].apply(str.title)
-        ## Undo the below lines to get a list of the unique question numbers for OCR
-        print(ocr_coll)
-        mylist = sorted(df['Question Number'].unique())
-        print(mylist)
-        print('\n')
-
-
-        db_dfs[ocr_coll] = df # Create a df and add it to the dict
-
-    for df_name in db_dfs.keys():
-        print('Loading '+df_name)
+        print('Loading '+ocr_coll)
         # If the collection doesnt exist or if the update is forced
 
-        if conn.collection_existence_check(DB_NAME, df_name)==False or force_update:
-            collection = conn.get_db_collection(DB_NAME, df_name)
+        if conn.collection_existence_check(DB_NAME, ocr_coll)==False or force_update:
+            collection = conn.get_db_collection(DB_NAME, ocr_coll)
 
             # Get the dataframe
-            df = db_dfs[df_name]
+            df = db_dfs[ocr_coll]
 
             # Delete all of the current contents from the collection
             collection.delete_many({})
-            if df_name == 'CAaS':
+            if ocr_coll == 'CAaS':
                 for i in [1,2,3,4]: # Splits df into 4 parts for uploading without AutoReconnect Error, especially for 
                     # load the db for the given data file into a json format
                     records = df[(i-1)*int(len(df)/4):i*int(len(df)/4)].to_dict('records')
@@ -75,19 +68,19 @@ def update_database(force_update=False):
                 result = collection.insert_many(records)
 
             # Update the user on what happened
-            print('A collection called ' + df_name + ' was added to the database '+ DB_NAME + '.')
+            print('A collection called ' + ocr_coll + ' was added to the database '+ DB_NAME + '.')
 
         else:
-            print('A collection called ' + df_name + ' already exists in the database '+ DB_NAME + ' and was unmodified.')
+            print('A collection called ' + ocr_coll + ' already exists in the database '+ DB_NAME + ' and was unmodified.')
             
         # Check to see if the aggregated document already exists in the document in the database
-        if conn.collection_existence_check(DB_NAME, 'aggregated_' + df_name)==False or force_update:
-            collection = conn.get_db_collection(DB_NAME, 'aggregated_' + df_name)
+        if conn.collection_existence_check(DB_NAME, 'aggregated_' + ocr_coll)==False or force_update:
+            collection = conn.get_db_collection(DB_NAME, 'aggregated_' + ocr_coll)
             # Get the dataframe
-            df = db_dfs[df_name]
+            df = db_dfs[ocr_coll]
 
             # Create the aggregated database 
-            print('Aggregating the ' + df_name + '. This usually takes approximately 1 minute, though can take longer for large datasets.')
+            print('Aggregating the ' + ocr_coll + '. This usually takes approximately 1 minute, though can take longer for large datasets.')
             ag_df = aggregate_data(df)
 
             # load the db for the given data file into a json format
@@ -100,10 +93,10 @@ def update_database(force_update=False):
             ag_result = collection.insert_many(ag_records)
 
             # Update the user on what happened
-            print('A collection called aggregated_'+ df_name + ' was added to the database '+ DB_NAME + '.')
+            print('A collection called aggregated_'+ ocr_coll + ' was added to the database '+ DB_NAME + '.')
 
         else:
-            print('A collection called aggregated_'+ df_name + ' already exists in the database '+ DB_NAME + ' and was unmodified.')
+            print('A collection called aggregated_'+ ocr_coll + ' already exists in the database '+ DB_NAME + ' and was unmodified.')
             
     # Return the connection to the collection
     return conn
